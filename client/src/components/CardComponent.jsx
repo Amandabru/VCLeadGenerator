f343037c21eee8afcbc3525b8944763430d11f9a
Amanda Brundin <abrundin@kth.se>
2023-10-25 16:39:28 +0200

    animation for employee

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -6,9 +6,16 @@ import Typography from '@mui/joy/Typography';
 import './CardComponent.css';
 import { CgProfile } from 'react-icons/cg';
 import { MdOutlinePlace } from 'react-icons/md';
-
+import { useState } from 'react';
 
 function CardComponent() {
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleNameClick = () => {
+      setIsClicked(true);
+    }
+
     return (
         <Card
         size="lg"
@@ -24,7 +31,7 @@ function CardComponent() {
         }}
       >
         <CardOverflow
-          className="left-side"
+          className={`${isClicked ? 'left-side2' : 'left-side'}`}
           sx={{
             flex: '0 0 30rem',
             display: 'flex',
@@ -33,43 +40,47 @@ function CardComponent() {
             px: 'var(--Card-padding)',
           }}
         >
-        <Typography fontFamily="'Poppins', sans-serif" fontSize="xl4" fontWeight="xl" textColor="#fff">
-            QuantumFleet
-        </Typography>
-        <AspectRatio sx={{ mt: 1 }} ratio="19/8" objectFit="contain" variant="plain">
-            <img
-              alt=""
-              src="https://static.vecteezy.com/system/resources/previews/006/409/485/original/people-thinking-to-make-decision-problem-solving-and-find-creative-ideas-with-question-mark-in-flat-cartoon-background-for-poster-illustration-vector.jpg"
-            />
-        </AspectRatio>
-        <div style={{display: 'flex', alignItems: 'center', }}>
-            <Typography className="tag" fontSize="0.8rem" fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 5, marginRight: '1rem' }}>
-                TRANSPORTATION
-            </Typography>
-            <Typography fontSize="0.8rem" fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 5, marginLeft: '8rem', marginRight: '1rem' }}>
-                <MdOutlinePlace size={15}/> Stockholm
-            </Typography>
-            <Typography fontSize="0.8rem" fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 5}}>
-                Founded in 2022
-            </Typography>
-        </div>
-        <Typography fontSize="0.9rem" fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 2}}>
-            QuantumFleet is revolutionizing the transportation industry by developing a fleet of quantum-powered vehicles. 
-            Our cutting-edge quantum technology allows for near-instantaneous travel between locations, 
-            drastically reducing commute times and carbon emissions.
-        </Typography>
-        <Typography fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 4 }}>
-            <a href="">
-                Link to Website
-            </a>
-        </Typography>
+        {!isClicked && (
+            <div>
+                <Typography fontFamily="'Poppins', sans-serif" fontSize="xl4" fontWeight="xl" textColor="#fff">
+                    QuantumFleet
+                </Typography>
+                <AspectRatio sx={{ mt: 1 }} ratio="19/8" objectFit="contain" variant="plain">
+                    <img
+                    alt=""
+                    src="https://static.vecteezy.com/system/resources/previews/006/409/485/original/people-thinking-to-make-decision-problem-solving-and-find-creative-ideas-with-question-mark-in-flat-cartoon-background-for-poster-illustration-vector.jpg"
+                    />
+                </AspectRatio>
+                <div style={{display: 'flex', alignItems: 'center', }}>
+                    <Typography className="tag" fontSize="0.8rem" fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 5, marginRight: '1rem' }}>
+                        TRANSPORTATION
+                    </Typography>
+                    <Typography fontSize="0.8rem" fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 5, marginLeft: '8rem', marginRight: '1rem' }}>
+                        <MdOutlinePlace size={15}/> Stockholm
+                    </Typography>
+                    <Typography fontSize="0.8rem" fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 5}}>
+                        Founded in 2022
+                    </Typography>
+                </div>
+                <Typography fontSize="0.9rem" fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 2}}>
+                    QuantumFleet is revolutionizing the transportation industry by developing a fleet of quantum-powered vehicles. 
+                    Our cutting-edge quantum technology allows for near-instantaneous travel between locations, 
+                    drastically reducing commute times and carbon emissions.
+                </Typography>
+                <Typography fontFamily="'Poppins', sans-serif" textColor="#fff" sx={{ mt: 4 }}>
+                    <a href="">
+                        Link to Website
+                    </a>
+                </Typography>
+            </div>
+            )}
         </CardOverflow>
         <CardContent sx={{ gap: 1.5, minWidth: 200 }}>
           <CardContent>
             <Typography textColor="#fff" sx={{ mt: 7 }} fontFamily="'Poppins', sans-serif" className="employees-title" level="title-lg">Employees  -4</Typography>
             <Typography fontSize="sm" sx={{ mt: 0.5 }}>
-            <ul className="custom-list">
-                <li> <CgProfile/><a href="#1"> Bo Tunnquist</a></li>
+            <ul className={`custom-list ${isClicked ? 'move-left' : ''}`}>
+                <li> <CgProfile/><a href="#1" onClick={handleNameClick}> Bo Tunnquist</a></li>
                 <li><CgProfile/><a href="#1"> Janne Jansson</a></li>
                 <li><CgProfile/><a href="#1"> Sofia Andersson</a></li>
                 <li><CgProfile/><a href="#1"> Kalle Karlsson</a></li>
