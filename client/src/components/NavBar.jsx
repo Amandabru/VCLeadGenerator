a52a5f0140c3d59fdd999a288b3e812d35f9f34c
Amanda Brundin <abrundin@kth.se>
2023-10-25 10:19:59 +0200

    Implemented navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,23 @@
+import { Link } from "react-router-dom";
+import './NavBar.css';
+
 function NavBar() {
+ 
     return (
-        <div>
-            <h1>
-                NavBar
-            </h1>
-        </div>
+       <nav>
+          <Link to="/" className="title">Name</Link>
+        <ul>
+            <li>
+                <Link to="/explore">Explore</Link>
+            </li>
+            <li>
+                <Link to="/saved">Saved</Link>
+            </li>
+            <li>
+                <Link to="/profile">Profile</Link>
+            </li>
+        </ul>
+       </nav>
     );
 }
 
